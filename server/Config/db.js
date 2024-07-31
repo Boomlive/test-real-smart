d4da5e0e0019e20816c1c420ea11e5ecf5fe62d7
panuwith <boomlivecup@gmail.com>
2024-07-31 23:45:35 +0700

    config env mongodb

--- server/Config/db.js
+++ server/Config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test-real-smart");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("DB Connected");
   } catch (err) {
     console.log(err);
